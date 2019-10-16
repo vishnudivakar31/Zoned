4871629eaf575ca2d92137f8cd1c167ed47d1a9e
Vishnu Divakar <vishnudivakar31@gmail.com>
2019-10-16 17:23:08 -0400

    navigation bar initial commit

--- index.php
+++ index.php
@@ -15,93 +15,133 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Zoned</title>
+    <link rel="icon" href="assets/images/logo.png">
   </head>
   <body>
 
-    <div class="nowPlayingBarContainer">
-      <div class="nowPlayingBar">
-        <div class="nowPlayingBarLeft">
-          <div class="content">
+    <div class="mainContainer">
 
-            <span class="albumLink">
-              <img src="http://clipart-library.com/img/1250259.png" alt="album" class="albumArtwork">
-            </span>
+      <div class="topContainer">
+        <div class="navBarContainer">
+          <nav class="navBar">
 
-            <div class="trackInfo">
+            <a href="index.php" class="logo">
+              <img src="assets/images/logo.png" alt="logo">
+            </a>
 
-              <span class="trackName">
-                <span>Happy Birthday</span>
-              </span>
+            <div class="group">
 
-              <span class="artistName">
-                <span>Vishnu Divakar</span>
-              </span>
+              <div class="navItem">
+                <a href="search.php" class="navItemLink">Search</a>
+                <img src="assets/images/icons/search.png" alt="search-icon" class="searchIcon">
+              </div>
 
             </div>
 
-          </div>
+            <div class="group">
+              <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+              </div>
+
+              <div class="navItem">
+                <a href="yourMusic.php" class="navItemLink">Your Music</a>
+              </div>
+
+              <div class="navItem">
+                <a href="profile.php" class="navItemLink">Profile</a>
+              </div>
+            </div>
+
+          </nav>
         </div>
-        <div class="nowPlayingBarCenter">
-          <div class="content playerControls">
+      </div>
 
-            <div class="buttons">
+      <div class="nowPlayingBarContainer">
+        <div class="nowPlayingBar">
+          <div class="nowPlayingBarLeft">
+            <div class="content">
 
-              <button class="controlButton shuffle" title="shuffle">
-                <img src="assets/images/icons/shuffle.png" alt="shuffle" />
-              </button>
+              <span class="albumLink">
+                <img src="https://www.doc.gold.ac.uk/creativeprojects/wp-content/uploads/2016/10/unnamed.jpg" alt="album" class="albumArtwork">
+              </span>
 
-              <button class="controlButton previous" title="previous">
-                <img src="assets/images/icons/previous.png" alt="previous" />
-              </button>
+              <div class="trackInfo">
 
-              <button class="controlButton play" title="play">
-                <img src="assets/images/icons/play.png" alt="play" />
-              </button>
+                <span class="trackName">
+                  <span>Happy Birthday</span>
+                </span>
 
-              <button class="controlButton pause" title="pause" style="display: none;">
-                <img src="assets/images/icons/pause.png" alt="pause" />
-              </button>
+                <span class="artistName">
+                  <span>Vishnu Divakar</span>
+                </span>
 
-              <button class="controlButton next" title="next">
-                <img src="assets/images/icons/next.png" alt="next" />
-              </button>
-
-              <button class="controlButton repeat" title="repeat">
-                <img src="assets/images/icons/repeat.png" alt="repeat" />
-              </button>
+              </div>
 
             </div>
+          </div>
+          <div class="nowPlayingBarCenter">
+            <div class="content playerControls">
 
-            <div class="playbackBar">
+              <div class="buttons">
 
-              <span class="progressTime current">0:00</span>
+                <button class="controlButton shuffle" title="shuffle">
+                  <img src="assets/images/icons/shuffle.png" alt="shuffle" />
+                </button>
+
+                <button class="controlButton previous" title="previous">
+                  <img src="assets/images/icons/previous.png" alt="previous" />
+                </button>
+
+                <button class="controlButton play" title="play">
+                  <img src="assets/images/icons/play.png" alt="play" />
+                </button>
+
+                <button class="controlButton pause" title="pause" style="display: none;">
+                  <img src="assets/images/icons/pause.png" alt="pause" />
+                </button>
+
+                <button class="controlButton next" title="next">
+                  <img src="assets/images/icons/next.png" alt="next" />
+                </button>
+
+                <button class="controlButton repeat" title="repeat">
+                  <img src="assets/images/icons/repeat.png" alt="repeat" />
+                </button>
+
+              </div>
+
+              <div class="playbackBar">
+
+                <span class="progressTime current">0:00</span>
+                <div class="progressBar">
+                  <div class="progressBarBg">
+                    <div class="progress"></div>
+                  </div>
+                </div>
+                <span class="progressTime remaining">0:00</span>
+
+              </div>
+
+            </div>
+          </div>
+          <div class="nowPlayingBarRight">
+            <div class="volumeBar">
+
+              <button class="controlButton volume" title="volume">
+                <img src="assets/images/icons/volume.png" alt="volume">
+              </button>
+
               <div class="progressBar">
                 <div class="progressBarBg">
                   <div class="progress"></div>
                 </div>
               </div>
-              <span class="progressTime remaining">0:00</span>
 
             </div>
-
-          </div>
-        </div>
-        <div class="nowPlayingBarRight">
-          <div class="volumeBar">
-
-            <button class="controlButton volume" title="volume">
-              <img src="assets/images/icons/volume.png" alt="volume">
-            </button>
-
-            <div class="progressBar">
-              <div class="progressBarBg">
-                <div class="progress"></div>
-              </div>
-            </div>
-
           </div>
         </div>
       </div>
     </div>
+
   </body>
 </html>
--- register.php
+++ register.php
@@ -22,8 +22,8 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="assets/js/register.js"/>
-
     </script>
+    <link rel="icon" href="assets/images/logo.png">
   </head>
   <body>
     <?php
